4f47a5820ca2659593a8f0c7788b7199e7318dbe
somotto <stephenonyango4783@gmail.com>
2024-08-05 11:42:39 +0300

    feat: register and serve the handlers

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"ascii/handlers"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+    if len(os.Args) != 1 {
+        return
+    }
+
+    // Register the handler to serve static files
+    http.HandleFunc("/static/", handlers.ServeStatic)
+
+    // Register the handler for the home page.
+    http.HandleFunc("/", handlers.HomeHandler)
+
+    // Register the handler for the ASCII art generation.
+    http.HandleFunc("/ascii-art", handlers.AsciiArtHandler)
+
+    port := ":8000"
+
+    fmt.Printf("Server is running on http://localhost%s", port)
+
+    // Start the HTTP server and log any errors.
+    log.Fatal(http.ListenAndServe(port, nil))
+}
