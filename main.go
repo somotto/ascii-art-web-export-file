cdbdce26796c5879f29e2cb45e95d4cb81360840
hanapiko <apikojuma94@gmail.com>
2024-08-06 12:45:31 +0300

    style: formatting main file

--- main.go
+++ main.go
@@ -9,23 +9,23 @@ import (
 )
 
 func main() {
-    if len(os.Args) != 1 {
-        return
-    }
+	if len(os.Args) != 1 {
+		return
+	}
 
-    // Register the handler to serve static files
-    http.HandleFunc("/static/", handlers.ServeStatic)
+	// Register the handler to serve static files
+	http.HandleFunc("/static/", handlers.ServeStatic)
 
-    // Register the handler for the home page.
-    http.HandleFunc("/", handlers.HomeHandler)
+	// Register the handler for the home page.
+	http.HandleFunc("/", handlers.HomeHandler)
 
-    // Register the handler for the ASCII art generation.
-    http.HandleFunc("/ascii-art", handlers.AsciiArtHandler)
+	// Register the handler for the ASCII art generation.
+	http.HandleFunc("/ascii-art", handlers.AsciiArtHandler)
 
-    port := ":8000"
+	port := ":8000"
 
-    fmt.Printf("Server is running on http://localhost%s", port)
+	fmt.Printf("Server is running on http://localhost%s", port)
 
-    // Start the HTTP server and log any errors.
-    log.Fatal(http.ListenAndServe(port, nil))
+	// Start the HTTP server and log any errors.
+	log.Fatal(http.ListenAndServe(port, nil))
 }
