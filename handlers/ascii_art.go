2988a8ec1f3f3040111a38680fa3cba3ca747dbe
weakinyi <akinyiwendy46@gmail.com>
2024-08-07 14:12:49 +0300

    chore: remove new line characters from error messages

--- handlers/ascii_art.go
+++ handlers/ascii_art.go
@@ -13,7 +13,7 @@ type PageData struct {
 
 func AsciiArtHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
-		renderErrorPage(w, "405\nMethod Not Allowed", http.StatusMethodNotAllowed)
+		renderErrorPage(w, "405 Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
@@ -21,22 +21,22 @@ func AsciiArtHandler(w http.ResponseWriter, r *http.Request) {
 	banner := r.FormValue("banner")
 
 	if !functions.InputString(text) {
-		renderErrorPage(w, "400\nBad Request: Input contains non-ASCII characters", http.StatusBadRequest)
+		renderErrorPage(w, "400 Bad Request: Input contains non-ASCII characters", http.StatusBadRequest)
 		return
 	}
 
 	fileName := banner + ".txt"
 	if functions.FileName(fileName) == "" {
-		renderErrorPage(w, "404\nNot Found: Invalid banner", http.StatusNotFound)
+		renderErrorPage(w, "404 Not Found: Invalid banner", http.StatusNotFound)
 		return
 	}
 
 	lines, err := functions.Readfile(fileName)
 	if err != nil {
 		if errors.Is(err, functions.ErrFileMissing) {
-			renderErrorPage(w, "404\nNot Found: Banner file missing", http.StatusNotFound)
+			renderErrorPage(w, "404 Not Found: Banner file missing", http.StatusNotFound)
 		} else {
-			renderErrorPage(w, "500\nInternal Server Error: Unable to process banner file", http.StatusInternalServerError)
+			renderErrorPage(w, "500 Internal Server Error: Unable to process banner file", http.StatusInternalServerError)
 		}
 		return
 	}
@@ -45,7 +45,7 @@ func AsciiArtHandler(w http.ResponseWriter, r *http.Request) {
 
 	tmpl, err := template.ParseFiles("templates/index.html")
 	if err != nil {
-		renderErrorPage(w, "500\nInternal Server Error", http.StatusInternalServerError)
+		renderErrorPage(w, "500 Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 
@@ -57,7 +57,7 @@ func renderErrorPage(w http.ResponseWriter, message string, statusCode int) {
 	w.WriteHeader(statusCode)
 	tmpl, err := template.ParseFiles("templates/error.html")
 	if err != nil {
-		http.Error(w, "500\nInternal Server Error", http.StatusInternalServerError)
+		http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 	tmpl.Execute(w, struct{ ErrorMessage string }{ErrorMessage: message})
